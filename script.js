ec8464234796fb7dc8073d1c518ecb2ba875e660
Antoxa <ant.oxa@bk.ru>
2021-01-09 23:21:22 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+//Создание объекта
+const obj = {
+  name: "John",
+  age: 25,
+  isMarried: false
+};
+
+const human = {
+  name: "Anton",
+  age: 30,
+  sex: "man"
+};
+
+console.log(obj.name);
+
+//Создание массива
+let arrColor = ['orange', 'blue', 'green'];
+console.log(arrColor[1]);
+
+//Интерполяция
+const category = "toys";
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = "АнтоХА";
+
+alert(`Привет, ${user}!`);
